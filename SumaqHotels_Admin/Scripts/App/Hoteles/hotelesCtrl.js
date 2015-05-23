a402a879b0979fa0ee10ae79ee0ca271b881a2da
iafar86 <iafar86@hotmail.com>
2015-05-23 16:02:32 -0300

    Vista para el Alta de hotel con su controlador (falta imagen del asset)

--- SumaqHotels_Admin/Scripts/App/Hoteles/hotelesCtrl.js
+++ SumaqHotels_Admin/Scripts/App/Hoteles/hotelesCtrl.js
@@ -1,4 +1,55 @@
-﻿sumaqHotelsApp.controller('hotelesCtrl', function ($scope, $stateParams, $state, $filter, ngTableParams, hotelesDataFactory, listadoHoteles, infoHoteles) {
+﻿sumaqHotelsApp.controller('hotelesCtrl', function ($scope, $stateParams, $state, $filter, ngTableParams, hotelesDataFactory)
+//, listadoHoteles, infoHoteles
+{
+     $scope.oneAtATime = true;
+     $scope.Hotel = [];
+     $scope.Hotel.Stars = 1;
+  $scope.groups = [
+    {
+      title: 'Dynamic Group Header - 1',
+      content: 'Dynamic Group Body - 1'
+    },
+    {
+      title: 'Dynamic Group Header - 2',
+      content: 'Dynamic Group Body - 2'
+    }
+  ];
+
+  $scope.items = ['Item 1', 'Item 2', 'Item 3'];
+
+  $scope.addItem = function() {
+    var newItemNo = $scope.items.length + 1;
+    $scope.items.push('Item ' + newItemNo);
+  };
+
+  $scope.status = {
+    isFirstOpen: true,
+    isFirstDisabled: false
+  };
+
+    //manejo de Estrellas de Hotel
+  $scope.hoveringOver = function (value) {
+      $scope.overStar = value;
+  };
+
+    //Manejo de Carrousel
+
+  $scope.myInterval = 5000;
+  var slides = $scope.slides = [];
+  $scope.addSlide = function (i) {
+      var newWidth = 6 + i;
+      slides.push({
+          //image: 'http://placekitten.com/' + newWidth + '/300',
+          image: 'http://q-ec.bstatic.com/images/hotel/840x460/420/4204142' + newWidth + '.jpg',
+          text: ['Lobby', 'Reception', 'Main', 'Confort'][slides.length % 4] + ' ' +
+            ['Place', 'Bar', 'Rooms', 'Beds'][slides.length % 4]
+      });
+  };
+  for (var i = 0; i < 4; i++) {
+      $scope.addSlide(i);
+  }
+
+
 
 
 });
